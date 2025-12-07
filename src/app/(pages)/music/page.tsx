58b82455ae8a4e10752a132ed23de97c3a66be58
leerobertdyer <leerobertdyer@gmail.com>
2025-12-06 22:25:27 -0500

    YoutubeLite component adding videos

--- src/app/(pages)/music/page.tsx
+++ src/app/(pages)/music/page.tsx
@@ -1,18 +1,54 @@
 import LinkPhoto from "@/components/LinkPhoto/LinkPhoto";
+import YouTubeLite from "@/components/YouTube/YouTube";
 
 export default function Music() {
-    return (
-        <div className="flex flex-col md:flex-row justify-evenly items-center mt-4 gap-4 flex-wrap">
+  return (
+    <>
+      <div className="flex flex-col md:flex-row justify-evenly items-center mt-4 gap-4 flex-wrap bg-black pb-[3rem]">
+        <h2 className="mt-4 pb-4 w-full text-center border-b-2 border-leeWhite-base">Band Websites</h2>
+        <LinkPhoto
+          src="/images/auntVickiTristan.png"
+          alt="Aunt Vicki Band"
+          text="Aunt Vicki"
+          linkto="https://www.auntvicki.rocks/"
+          target="blank"
+        />
 
-            <LinkPhoto src="/images/auntVickiTristan.png" alt="Aunt Vicki Band" 
-                text="Aunt Vicki" linkto="https://www.auntvicki.rocks/" target="blank"/>
+        <LinkPhoto
+          src="/images/wifeIslandLake.jpg"
+          alt="Wife Island Duo"
+          text="Aunt Vicki (Acoustic Duo)"
+          linkto="https://www.auntvicki.rocks/duo"
+          target="blank"
+        />
 
-            <LinkPhoto src="/images/wifeIslandLake.jpg" alt="Wife Island Duo" 
-                text="Wife Island (AKA Aunt Vicki Duo)" linkto="https://wife-island-git-main-leerobertdyers-projects.vercel.app/" target="blank"/>
+        <LinkPhoto
+          src="/images/tinySunPiano.png"
+          alt="Wife Island Duo"
+          text="Tiny Sun"
+          linkto="https://www.tinysunstudio.com/"
+          target="blank"
+        />
+      </div>
+      <div id="videos" className="flex flex-col md:flex-row justify-evenly items-center mt-4 gap-4 flex-wrap bg-black pb-[3rem]">
+        <h2 className="mt-4 pb-4 w-full text-center border-b-2 border-leeWhite-base">Videos</h2>
 
-            <LinkPhoto src="/images/tinySunPiano.png" alt="Wife Island Duo" 
-                text="Tiny Sun" linkto="https://www.tinysunstudio.com/" target="blank"/>
+        <h3 className="mt-4 w-full text-center">Aunt Vicki</h3>
+        <YouTubeLite id="Yg_q40mY48c" title='Original - Lights Out'/>
+        <YouTubeLite id="SrD2nilSt2I" title='Original - Vigil'/>
+        <YouTubeLite id="aiFS5uEtRlA" title='Original - Time Is On Your Side'/>
+        
+        <h3 className="mt-4 w-full text-center">Tiny Sun</h3>
+        <YouTubeLite id="77kikByulrw" title='Cover - Elliot Smith "Memory Lane"'/>        
+        <YouTubeLite id="JupdC5IBrT8" title='Original - The Golden Hour'/>
+        <YouTubeLite id="sqLORIPKybk" title='Original - Emily Rose'/>
+        <YouTubeLite id="C-dldzvBR7I" title='Cover - CCR - "Lodi"'/>
+        <YouTubeLite id="cxlv6LJ3VaY" title='Cover - Johnny Cash - "I Walk The Line"'/>
 
-        </div>
-    );
-}   
+        <h3 className="mt-4 w-full text-center">Aunt Vicki Duo</h3>
+        <YouTubeLite id="IHQknZV8hLA" title='Original - Out Of My Mind'/>
+        <YouTubeLite id="QMCSdp_kxps" title='Original - Body Like A Cave'/>
+      </div>
+    </>
+  );
+}
---
+++ src/components/YouTube/YouTube.tsx
@@ -0,0 +1,33 @@
+"use client";
+// components/YouTubeLite.js
+import { useState } from "react";
+
+export default function YouTubeLite({ id, title }: { id: string, title: string }) {
+  const [load, setLoad] = useState(false);
+
+  return load ? (
+    <iframe
+      width="560"
+      height="315"
+      src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+      title="YouTube video"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  ) : (
+    <div
+      onClick={() => setLoad(true)}
+      style={{
+        backgroundImage: `url(https://i.ytimg.com/vi/${id}/hqdefault.jpg)`,
+      }}
+      className="w-[560px] h-[315px] aspect-video bg-cover bg-center cursor-pointer relative border-2 rounded-md"
+    >
+      <div className="absolute top-0 right-0 py-2 text-center z-90 bg-black bg-opacity-100 w-full">
+        {title}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center text-white text-5xl bg-black bg-opacity-40 rounded-md">
+        ▶
+      </div>
+    </div>
+  );
+}
