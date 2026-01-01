38992c6f4b6a930796756e32f2ed21268120e2dc
leerobertdyer <leerobertdyer@gmail.com>
2026-01-01 16:54:35 -0500

    feat: adding hidden image for instagram share

--- src/app/(pages)/music/page.tsx
+++ src/app/(pages)/music/page.tsx
@@ -1,9 +1,42 @@
 import LinkPhoto from "@/components/LinkPhoto/LinkPhoto";
 import YouTubeLite from "@/components/YouTube/YouTube";
 
+export const metadata = {
+  title: "Lee Dyer - Music",
+  description: "Lee Dyer - Alt/Acoustic/Folk/Indie",
+  openGraph: {
+    title: "Lee Dyer - Music",
+    description: "Lee Dyer - Alt/Acoustic/Folk/Indie",
+    url: "https://www.leedyer.com/music",
+    images: [
+      {
+        url: "https://www.leedyer.com/images/leeStage1.png",
+        width: 1200,
+        height: 630,
+        alt: "Lee Dyer Singing",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lee Dyer - Music",
+    description: "Lee Dyer - Alt/Acoustic/Folk/Indie",
+    images: [
+      "https://www.leedyer.com/images/leeStage1.png",
+      "https://www.leedyer.com/images/leeBlueKneeling.jpg",
+    ],
+  },
+};
+
 export default function Music() {
   return (
     <>
+      {/* Hidden OG image for Instagram scraping */}
+      <img
+        src="/images/leeStage1.png"
+        alt="Lee Dyer Solo"
+        style={{ display: "none" }}
+      />
       <div className="flex flex-col md:flex-row justify-evenly items-center  gap-2 flex-wrap bg-black py-[3rem] border-b-2 border-myWhite-base">
         <LinkPhoto
           size="w-[15rem] h-[15rem]"
@@ -41,10 +74,16 @@ export default function Music() {
         id="videos"
         className="flex flex-col md:flex-row justify-evenly items-center gap-4 flex-wrap bg-black pb-[3rem] border-b-2 border-myWhite-base pt-8"
       >
-        <YouTubeLite id="Yg_q40mY48c" title='Original - Aunt Vicki "Lights Out"' />
+        <YouTubeLite
+          id="Yg_q40mY48c"
+          title='Original - Aunt Vicki "Lights Out"'
+        />
         <YouTubeLite id="Pg2Ox9pJlvM" title='Original - Tiny Sun "Ageless"' />
         <YouTubeLite id="SrD2nilSt2I" title='Original - Aunt Vicki "Vigil"' />
-        <YouTubeLite id="aiFS5uEtRlA" title='Original - Aunt Vicki "Time Is On Your Side"' />
+        <YouTubeLite
+          id="aiFS5uEtRlA"
+          title='Original - Aunt Vicki "Time Is On Your Side"'
+        />
       </div>
     </>
   );
